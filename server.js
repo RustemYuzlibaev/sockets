03dcb8b0def4b265d3cf7d72f8abf62adbea0da1
Rustem <rustem.yuzlibaev@yandex.ru>
2018-04-13 12:03:15 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const socket = require('socket.io');
+
+// App setup
+const app = express();
+const server = app.listen(4000, () => {
+  console.log('listening to requests on port 4000');
+})
+// Static files
+app.use(express.static('public'));
+// Socket setup
+let io = socket(server);
+io.on('connection', (socket) => {
+  console.log('made socket connection');
+
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data);
+  });
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data)
+  })
+});
